a7d3fd4b3567b620366b3a12d725a1eee633dc70
calgil <clgilbert1105@gmail.com>
2023-03-27 09:19:38 -0600

    add router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>Error!!</div>,
+    children: [
+      {
+        errorElement: <div>Error!!</div>,
+        children: [
+          {
+            path: "/",
+            // element dashboard
+            // protected route
+          },
+          {
+            path: "login",
+            // element login
+            // unprotected route
+          },
+          {
+            path: "register",
+            // element register
+            // unprotected route
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
